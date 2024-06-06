39f44933c789b80390bc887a499945eb44d09cd9
bemadeveloper <akbermet1990@icloud.com>
2024-06-06 18:08:50 +0600

    Initial Commit

---
+++ TicketApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TicketApp
+//
+//  Created by Bema on 6/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
